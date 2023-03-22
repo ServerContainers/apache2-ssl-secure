1c760ada708a33e381d983773752adafb7cb3270
Marvin Winkler <m@rvin.at>
2023-03-22 17:39:26 +0100

    small fix

--- get-version.sh
+++ get-version.sh
@@ -2,7 +2,7 @@
 export IMG=$(docker build -q --pull --no-cache -t 'get-version' .)
 
 export DEBIAN_VERSION=$(docker run --rm -t get-version cat /etc/debian_version | tail -n1 | tr -d '\r')
-export APACHE_VERSION=$(docker run --rm -t get-version dpkg --list apache2 | | grep '^ii' | tr ' ' '\n' | grep '[0-9]\.[0-9]'| sed 's/[+=~:]/_/g' | tr -d '\r')
+export APACHE_VERSION=$(docker run --rm -t get-version dpkg --list apache2 | grep '^ii' | tr ' ' '\n' | grep '[0-9]\.[0-9]'| sed 's/[+=~:]/_/g' | tr -d '\r')
 [ -z "$DEBIAN_VERSION" ] && exit 1
 
 export IMGTAG=$(echo "$1""d$DEBIAN_VERSION-a$APACHE_VERSION")
